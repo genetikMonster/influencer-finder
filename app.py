c2c3a970854a36faf596eb1bdd14bb52fe5669fc
Gaël Latouche <gaellatouche@Gaels-MacBook-Pro.local>
2026-04-09 14:55:52 +0400

    spacing

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def check_login():
 
     st.set_page_config(layout="wide")
 
-    col1, col2, col3 = st.columns([1, 2, 1])
+    col1, col2, col3 = st.columns([1, 1.2, 1])
 
     with col2:
         st.markdown("## Login")
